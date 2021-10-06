175722fa2ed278cb7ec5f75f259d7aebd0f7ad82
Sarah Fuoss <sfuoss@gmail.com>
2021-10-06 14:54:37 -0500

    Writing query text to get secrets based on clearance level.

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -6,14 +6,25 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-
+  const userId = req.user.id;
+  queryForClearance = `SELECT "id","clearance_level" FROM "user" WHERE "id"=$1;`
   pool
-    .query(`SELECT * FROM "secret";`)
-    .then((results) => res.send(results.rows))
+    .query(queryForClearance, [userId])
+
+    .then((results) => 
+    const clearanceLevel = (results.rows.clearance_level);
+    console.log(clearanceLevel);
+    
+    
+    
+    pool.query(`SELECT "content" FROM "secret" WHERE "secret"."secrecy_level" < $1 `, [clearanceLevel]).then(results => {
+      res.sendStatus(201);
+    })
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
 });
+
 
 module.exports = router;
